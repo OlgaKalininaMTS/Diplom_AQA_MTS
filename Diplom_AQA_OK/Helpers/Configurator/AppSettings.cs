cb8d2e8fda9956afd2d645f1eec98ead2ea281ca
oa-kalinina@yandex.ru <oa-kalinina@yandex.ru>
2024-04-19 22:27:32 +0500

    commit

--- Diplom_AQA_OK/Helpers/Configurator/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Diplom_AQA_OK.Helpers.Configuration;
-
-public class AppSettings
-{
-    public string? URL { get; set; }
-    public string? Username { get; set; }
-    public string? Password { get; set; }
-}
