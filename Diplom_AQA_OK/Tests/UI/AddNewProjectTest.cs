9f4894cd8f0f5e88abb5736d78c9eef92496bb2c
oa-kalinina@yandex.ru <oa-kalinina@yandex.ru>
2024-04-25 18:47:43 +0500

    update broken file

--- Diplom_AQA_OK/Tests/UI/AddNewProjectTest.cs
+++ Diplom_AQA_OK/Tests/UI/AddNewProjectTest.cs
@@ -1,7 +1,4 @@
-﻿using Diplom_AQA_OK.Helpers;
-using Diplom_AQA_OK.Pages;
-using Diplom_AQA_OK.Steps;
-using OpenQA.Selenium;
+﻿using Diplom_AQA_OK.Pages;
 
 namespace Diplom_AQA_OK.Tests;
 
@@ -11,6 +8,18 @@ public class AddNewProjectTest : BaseTest
     [Order(1)]
     [Category("Smoke")]
     [Category("Regression")]
+    public void SuccessfulAddProjectTest()
+    {
+        Assert.That(_navigationSteps.SuccessfulLogin(Admin).TitleLabel.Displayed);
+
+        DashboardPage dashboardPage = new DashboardPage(Driver);
+
+        dashboardPage.SuccessFulAddNewProject("New Test Project", "123", "test");
+    }
+    [Test]
+    [Order(2)]
+    [Category("Smoke")]
+    [Category("Regression")]
     public void InvalidAddProjectTest()
     {
         Assert.That(_navigationSteps.SuccessfulLogin(Admin).TitleLabel.Displayed);
@@ -18,21 +27,5 @@ public class AddNewProjectTest : BaseTest
         DashboardPage dashboardPage = new DashboardPage(Driver);
 
         dashboardPage.IncorrectNewProject("Invalid Test Project", "123456789", "test");
-    }
-    
-    [Test]
-    [Order(2)]
-    [Category("Smoke")]
-    [Category("Regression")]
-    public void SuccessfulAddProjectTest()
-    {
-        Assert.That(_navigationSteps.SuccessfulLogin(Admin).TitleLabel.Displayed);
-
-        DashboardPage dashboardPage = new DashboardPage(Driver);
-
-        dashboardPage.SuccessFulAddNewProject("New Test Project","123","test");        
-    }
-    
-
-    
+    }        
 }
